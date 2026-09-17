fcb722bec238fd599652b59adfd1e396e0efb7a7
PetitJump <203864356+PetitJump@users.noreply.github.com>
2026-09-17 12:58:09 +0200

    Ajout d'une limite a la position des raquettes

--- src/move_raquette.c
+++ src/move_raquette.c
@@ -4,15 +4,27 @@
 
 void move_raquette(sfRectangleShape *raquette1, sfRectangleShape *raquette2)
 {
-    if(sfKeyboard_isKeyPressed(sfKeyZ))
-            sfRectangleShape_move(raquette1, (sfVector2f){0, -7}); // déplace de 5 pixels vers le haut
+        if (sfKeyboard_isKeyPressed(sfKeyZ)){
+            sfVector2f pos = sfRectangleShape_getPosition(raquette1);
+            if (pos.y >= 0)
+                sfRectangleShape_move(raquette1, (sfVector2f){0, -7}); // déplace de 5 pixels vers le haut
+        }
         
-        if(sfKeyboard_isKeyPressed(sfKeyS))
-            sfRectangleShape_move(raquette1, (sfVector2f){0, 7}); // déplace de 5 pixels vers le bas
+        if (sfKeyboard_isKeyPressed(sfKeyS)){
+            sfVector2f pos = sfRectangleShape_getPosition(raquette1);
+            if (pos.y <=1100)
+                sfRectangleShape_move(raquette1, (sfVector2f){0, 7}); // déplace de 5 pixels vers le bas
+        }
 
-        if(sfKeyboard_isKeyPressed(sfKeyUp))
-            sfRectangleShape_move(raquette2, (sfVector2f){0, -7}); // déplace de 5 pixels vers le haut
-        
-        if(sfKeyboard_isKeyPressed(sfKeyDown))
-            sfRectangleShape_move(raquette2, (sfVector2f){0, 7}); // déplace de 5 pixels vers le bas
+        if (sfKeyboard_isKeyPressed(sfKeyUp)){ 
+            sfVector2f pos = sfRectangleShape_getPosition(raquette2);
+            if (pos.y >= 0)
+                sfRectangleShape_move(raquette2, (sfVector2f){0, -7}); // déplace de 5 pixels vers le haut
+        }
+
+        if (sfKeyboard_isKeyPressed(sfKeyDown)){
+            sfVector2f pos = sfRectangleShape_getPosition(raquette2);
+            if (pos.y <= 1100)
+                sfRectangleShape_move(raquette2, (sfVector2f){0, 7}); // déplace de 5 pixels vers le bas
+        }
 }
